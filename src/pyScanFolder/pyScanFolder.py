454ba6f674be962a2e7fea0786a06b3fcb9d2933
rb <rb@elektronik.local>
2013-08-15 15:16:16 +0200

    initial commit

---
+++ src/pyScanFolder/pyScanFolder.py
@@ -0,0 +1,199 @@
+'''
+Created on 11.07.2013
+
+@author: rb
+'''
+
+import getopt
+import sys
+import os
+import sqlite3
+import ConfigParser
+import hashlib
+import pyexiv2
+
+
+ConfigFile='RBPicsDB.cfg'
+
+DbHost = ''
+DbDatabase = ''
+DbUser = ''
+DbPassword = ''
+
+
+verbindung = None
+def main():
+    argv = sys.argv
+    ScanDir = './'
+    NewTables = False
+    
+    try:
+        opts, _ = getopt.getopt(argv[1:], 'hf:nd:', ['help', 'file=', 'new', 'directory='])
+    except getopt.GetoptError as detail:
+        print detail
+        sys.exit(1)
+    
+    for opt, arg in opts :
+        if opt in ('-h', '--help'):
+            printHelp()
+        
+        if opt in ('-f', '--file'):
+            global ConfigFile
+            ConfigFile = str(arg)    
+
+        if opt in ('-n', '--new'):
+            NewTables = True
+            
+        if opt in ('-d', '--directory'):
+            ScanDir = str(arg)
+    
+    AbsScanDir = os.path.abspath(ScanDir)
+    
+    (DbHost, DbDatabase, DbUser, DbPassword) = ReadDatabaseSettings()
+    try:
+        global verbindung 
+        verbindung = sqlite3.connect(DbDatabase)
+
+    except sqlite3.DatabaseError as detail:
+        print detail
+        sys.exit(1)
+
+    if NewTables == True:
+        newTables()
+    
+    addPath2Db(AbsScanDir)
+    addBilder2Db(AbsScanDir, NewTables)
+    sys.exit(0)
+    
+def addBilder2Db(Pfad, EmptyDatabase = False):
+    daten = []
+    cursor = verbindung.cursor()
+    SQL = "SELECT ID FROM Verzeichnisse WHERE Pfad = '%s'" %Pfad
+    cursor.execute(SQL)
+    Antwort = cursor.fetchone()
+    PfadId = int(Antwort[0])
+    Dateien = os.listdir((Pfad+"/"))
+    Dateien.sort()
+    for Datei in Dateien:
+        if Datei.endswith(".jpg"):
+            if EmptyDatabase == False:
+                SQL="SELECT Count(*) FROM Bilder WHERE Name = '%s' AND Pfad = '%s' "%(Datei, PfadId)
+                cursor.execute(SQL)
+                Antwort = cursor.fetchone()
+            else:
+                Antwort = 0
+                
+            if Antwort == 0:
+                Md5 = md5ForFile('%s/%s'%(Pfad,Datei))
+                exif = pyexiv2.ImageMetadata('%s/%s'%(Pfad,Datei))
+                try :
+                    exif.read()
+                    (Width, Height) = exif.dimensions
+                except:
+                    Width = 0
+                    Height = 0
+                
+                datum = (Datei, PfadId, Width, Height, Md5)
+                daten.append(datum)
+                if len(daten) == 1000:
+                    addBild2Db(daten)
+                    daten = []
+    
+    if daten != None:        
+        addBild2Db(daten)
+
+def addBild2Db(Daten):
+    cursor = verbindung.cursor()
+    SQL="INSERT INTO Bilder(Name, Pfad, Width, Height, Md5Sum) VALUES(?, ?, ?, ?, ?)"
+    cursor.executemany(SQL, Daten)
+    verbindung.commit()
+
+def addPath2Db(Pfad):
+    cursor = verbindung.cursor()
+    SELECT = "SELECT * FROM Verzeichnisse WHERE Pfad = '%s'" %Pfad
+    cursor.execute(SELECT)
+    Antwort = cursor.fetchone()
+    if Antwort == None:
+        INSERT = "INSERT INTO Verzeichnisse(Pfad) VALUES('%s')" %Pfad
+        cursor.execute(INSERT)
+        verbindung.commit()
+        SELECT = "SELECT * FROM Verzeichnisse WHERE Pfad = '%s'" %Pfad
+        cursor.execute(SELECT)
+        Antwort = cursor.fetchone()
+        
+def ReadDatabaseSettings():
+    DbHost = ''
+    DbDatabase = ''
+    DbUser = ''
+    DbPassword = ''
+    
+    config = ConfigParser.ConfigParser()
+    try:
+        config.read(ConfigFile)
+    except ConfigParser.Error:
+        sys.exit(1)
+    
+    items = config.items('Database')
+    for Schluessel, Wert in items:
+        if Schluessel in 'host':
+            DbHost = Wert
+        if Schluessel in 'database':
+            DbDatabase = Wert
+        if Schluessel in 'user':
+            DbUser = Wert
+        if Schluessel in 'password':
+            DbPassword = Wert
+        
+    return(DbHost, DbDatabase, DbUser, DbPassword)
+    
+def newTables():
+    cursor = verbindung.cursor()
+    #Drop Table Bilder
+    SQL="DROP TABLE IF EXISTS Bilder"
+    cursor.execute(SQL)
+    #Drop Table Verzeichnisse
+    SQL="DROP TABLE IF EXISTS Verzeichnisse"
+    cursor.execute(SQL)
+    #Create Table Verzeichnisse
+    SQL="CREATE TABLE Verzeichnisse( \
+        ID INTEGER PRIMARY KEY AUTOINCREMENT,\
+        Pfad TEXT)"
+    cursor.execute(SQL)
+    #Create Table Bilder
+    SQL="CREATE TABLE Bilder(\
+        ID INTEGER PRIMARY KEY AUTOINCREMENT,\
+        Name TEXT,\
+        Pfad INTEGER,\
+        Width INTEGER,\
+        Height INTEGER,\
+        Depth INTEGER,\
+        MD5Sum TEXT,\
+        ViewCntr INTEGER NOT NULL DEFAULT 0,\
+        LastViewed DATE)"
+    cursor.execute(SQL)
+
+def newTableVerzeichnisse():
+    pass
+
+def newTableBild():
+    pass
+
+
+def printHelp():
+    pass
+    
+def md5ForFile(path, block_size=256*128):
+    '''
+    Block size directly depends on the block size of your filesystem
+    to avoid performances issues
+    Here I have blocks of 4096 octets (Default NTFS)
+    '''
+    md5 = hashlib.md5()
+    with open(path,'rb') as f: 
+        for chunk in iter(lambda: f.read(block_size), b''): 
+            md5.update(chunk)
+    return md5.hexdigest() 
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/pyUpdatePicture/pyUpdatePicture.py
@@ -0,0 +1,142 @@
+'''
+Created on 11.07.2013
+
+@author: rb
+'''
+
+import getopt
+import sys
+import os
+import sqlite3
+import ConfigParser
+import time
+import pyexiv2
+
+
+ConfigFile='RBPicsDB.cfg'
+
+DbHost = ''
+DbDatabase = ''
+DbUser = ''
+DbPassword = ''
+
+
+verbindung = None
+def main():
+    argv = sys.argv
+    ListDirs = False;
+    PicDirectory = './'
+    SleepTime = 60
+    LinkName = 'Bild.jpg' 
+    try:
+        opts, _ = getopt.getopt(argv[1:], 'hf:d:t:l:', ['help', 'file=', 'directory=', 'listdirs', 'time=', 'linkto='])
+    except getopt.GetoptError as detail:
+        print detail
+        sys.exit(1)
+    
+    for opt, arg in opts :
+        if opt in ('-h', '--help'):
+            printHelp()
+        
+        if opt in ('-f', '--file'):
+            global ConfigFile
+            ConfigFile = str(arg)    
+
+        if opt in ('-d', '--directory'):
+            PicDirectory = str(arg)
+            pass
+        
+        if opt in ('--listdirs'):
+            ListDirs = True
+    
+        if opt in ('-t', '--time'):
+            SleepTime = float(arg)
+        
+        if opt in ('-l', '--linkto'):
+            LinkName = str(arg)
+                
+    (DbHost, DbDatabase, DbUser, DbPassword) = ReadDatabaseSettings()
+    try:
+        global verbindung 
+        verbindung = sqlite3.connect(DbDatabase)
+    except sqlite3.DatabaseError as detail:
+        print detail
+        sys.exit(1)
+
+
+    if ListDirs == True:
+        Antwort = listDirs()
+        for Id, Verzeichnis in Antwort:
+            print "Verzeichnis: %s; ID: %d" %(Verzeichnis, Id)
+        sys.exit(0)
+
+    PicDirectory= os.path.abspath(PicDirectory)
+                
+    Antwort = getDirs("ID, Pfad", "WHERE Pfad='%s'" %PicDirectory)
+    Id, Verzeichnis = Antwort[0] 
+
+    (BildId, Bildname) = getBildFromDb(Id)
+    try:
+        os.remove(LinkName)
+    except:
+        pass
+    try:
+        os.symlink("%s/%s"%(Verzeichnis, Bildname), LinkName)
+    except:
+        print "Symlink konnte nicht erstellt werden!"
+    print "%s/%s hat die ID: %s" %(Verzeichnis, Bildname, BildId)
+    updateBildinfo(BildId)
+
+def updateBildinfo(BildId):
+    cursor = verbindung.cursor()
+    SQL = "UPDATE Bilder SET ViewCntr = ViewCntr + 1 WHERE ID = %s" %BildId
+    cursor.execute(SQL)
+    verbindung.commit()
+
+def getDirs(what="*", where=""):
+    cursor = verbindung.cursor()
+    SQL = "SELECT %s FROM Verzeichnisse %s"%(what,where)
+    cursor.execute(SQL)
+    Antwort = cursor.fetchall()
+    return Antwort
+    
+def listDirs():
+    return getDirs()
+
+def getBildFromDb(PfadId):
+    cursor = verbindung.cursor()
+    SQL = "SELECT Id, Name FROM Bilder WHERE Pfad = '%s' AND ViewCntr = (SELECT min(ViewCntr) FROM Bilder) ORDER by RANDOM() LIMIT 1" %PfadId
+    cursor.execute(SQL)
+    Antwort = cursor.fetchone()
+    return Antwort
+        
+def ReadDatabaseSettings():
+    DbHost = ''
+    DbDatabase = ''
+    DbUser = ''
+    DbPassword = ''
+    
+    config = ConfigParser.ConfigParser()
+    try:
+        config.read(ConfigFile)
+    except ConfigParser.Error:
+        sys.exit(1)
+    
+    items = config.items('Database')
+    for Schluessel, Wert in items:
+        if Schluessel in 'host':
+            DbHost = Wert
+        if Schluessel in 'database':
+            DbDatabase = Wert
+        if Schluessel in 'user':
+            DbUser = Wert
+        if Schluessel in 'password':
+            DbPassword = Wert
+        
+    return(DbHost, DbDatabase, DbUser, DbPassword)
+    
+def printHelp():
+    pass
+    
+if __name__ == '__main__':
+    main()
